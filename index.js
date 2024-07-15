7d131c3dbbf8a249030e2569680a2d320c1aa405
MD : Ridoy  Islam <162418372+programminsekbo@users.noreply.github.com>
2024-07-15 02:59:11 -0700

    Update index.js

--- index.js
+++ index.js
@@ -12,9 +12,26 @@ app.get("/six",function(req,res){
 });
 
 
+app.get("/six",function(req,res){
+    res.cookie("name","hridoy")
+    res.cookie("roll","50015")
+    res.cookie("city","dinajpur")
+    res.end("hello word")
+     
+ });
+
+ app.post("/",function(req,res){
+  
+   let fistName=req.header.fistName;
+   let lestName=req.header.lestName;
+    res.end( fistName+" "+lestName)
+     
+ });
+  
 
 
-app.listen( 2030, function() {
+
+app.listen( 3060, function() {
 
     console.log("server run success..")
 });
